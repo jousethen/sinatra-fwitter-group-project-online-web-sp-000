5d7f4d7b2b26f4201dbc86b7f8f2b4e88f88da55
Jouse then <jthen93@gmail.com>
2021-05-13 17:50:05 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,5 +13,54 @@ class TweetsController < ApplicationController
     end
   end
   
+  get '/tweets/new' do
+    if session[:user_id]
+      erb :'tweets/new'
+    else
+      redirect '/login'
+    end
+  end
+  
+  get '/tweets/:id' do
+    if session[:user_id]
+      @tweet = Tweet.find(params[:id])
+      erb :'tweets/show'
+    else
+      redirect '/login'
+    end
+  end
+  
+  get '/tweets/:id/edit' do
+    if session[:user_id]
+      @tweet = Tweet.find(params[:id])
+      
+      if @tweet.user_id == session[:user_id]
+       erb :'tweets/edit'
+     else
+       redirect "/tweets/#{@tweet.id}"
+     end
+     
+    else
+      redirect '/login'
+    end
+  end
+  
+  post '/tweets' do
+    if params[:content] == ""
+      redirect '/tweets/new'
+    else
+      user = User.find(session[:user_id])
+      tweet = Tweet.create(content: params[:content], user_id: user.id)
+      redirect "/tweets/#{tweet.id}"
+    end
+  end
+  
+  patch '/tweets/:id' do
+    tweet = Tweet.find(params[:id])
+    tweet.update(content: params[:content])
+    tweet.save
+    redirect "/tweets/#{tweet.id}"
+  end
+  
 
 end
